369d686d021f2ab45653bbf8118b4f3cbac8399a
Danil <danil.valeev@gmail.com>
2021-10-14 15:54:48 +0400

    Fixed experiments property (#4)
    * Handle default values in experimentsWithDetails function.
    
    * Updated example project.

---
+++ ProbaExample/ExperimentsService.swift
@@ -0,0 +1,56 @@
+//
+//  ExperimentsService.swift
+//  Proba
+//
+//  Created by Proba on 14.10.2021.
+//  Copyright © 2021 Proba. All rights reserved.
+//
+
+import Foundation
+
+struct ExperimentsService {
+  enum ButtonColor: String {
+    case red
+    case green
+    case yellow
+  }
+
+  enum Paywall: String {
+    case paywallA
+    case paywallB
+  }
+
+  /*private */let proba: Proba
+
+  // for test purposes; use some fixed value in production!
+  private let deviceId = String.random(ofLength: 8)
+
+  init() {
+    proba = Proba(
+      sdkToken: "7EF3877C105644C5ABEA3D8A53822CBE",
+      appId: "17598",
+      deviceId: deviceId,
+      usingShake: true,
+      defaults: [
+        "buttonColor": defaultButtonColor.rawValue,
+        "paywall": defaultPaywall.rawValue,
+      ]
+    )
+  }
+
+  func fetch(completion: @escaping (_ abError: ProbaABError?) -> Void) {
+    proba.fetch(completion: completion)
+  }
+
+  private let defaultButtonColor: ButtonColor = .green
+
+  var buttonColor: ButtonColor {
+    ButtonColor(rawValue: proba[#function] ?? "") ?? defaultButtonColor
+  }
+
+  private let defaultPaywall: Paywall = .paywallA
+
+  var paywall: Paywall {
+    Paywall(rawValue: proba[#function] ?? "") ?? defaultPaywall
+  }
+}
--- ProbaExample/ViewController.swift
+++ ProbaExample/ViewController.swift
@@ -9,28 +9,23 @@
 import UIKit
 
 class ViewController: ProbaShakeToDebugController {
+  let experimentsService = ExperimentsService()
 
   override func viewDidLoad() {
     super.viewDidLoad()
 
-    let ab = Proba(
-      sdkToken: "<YOUR_SDK_TOKEN>",
-      appId: "<YOUR_APP_ID>",
-      defaults: [
-        "<EXPERIMENT_1_KEY>": "<EXPERIMENT_1_DEFAULT_VALUE>",
-        "<EXPERIMENT_2_KEY>": "<EXPERIMENT_2_DEFAULT_VALUE>"
-      ]
-    )
+    experimentsService.fetch() { [unowned self] abError in
+      if let abError = abError {
+        print(abError.error)
 
-    ab.fetch() { abError in
-      guard abError == nil else { return }
+        return
+      }
 
-      let experiment1Value: String? = ab.value("<EXPERIMENT_1_KEY>")
-      let experiment2Value: String? = ab["<EXPERIMENT_2_KEY>"]
+      print("buttonColor: \(self.experimentsService.buttonColor)")
+      print("paywall: \(self.experimentsService.paywall)")
 
-      print(experiment1Value ?? "")
-      print(experiment2Value ?? "")
+      print("experiments: \(self.experimentsService.proba.experiments())")
+      print("experimentsWithDetails: \(self.experimentsService.proba.experimentsWithDetails())")
     }
   }
-
 }
